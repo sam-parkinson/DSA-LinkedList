cd8edb82fadc48d2a1862042d7bf4f036244316e
Sam Parkinson <parkinsonsp42@gmail.com>
2019-08-23 10:27:45 -0400

    Continue working drills

--- drills.js
+++ drills.js
@@ -15,13 +15,48 @@ function main() {
   return SLL
 }
 
-function display(linkedList) {};
+function display(linkedList) {
+  console.log(linkedList);
+};
 
-function size(linkedList) {};
+function size(linkedList) {
+  let size = 0;
+  let currNode = linkedList.head;
+  if (currNode === null) {
+    return size;
+  }
+  size = size + 1;
+  while (currNode.next !== null) {
+    size = size + 1;
+    currNode = currNode.next;
+  }
+  return size;
+};
 
-function isEmpty(linkedList) {};
+function isEmpty(linkedList) {
+  if (linkedList.head === null) {
+    return true;
+  }
+  return false;
+};
 
-function findLast(linkedList) {};
+function findLast(linkedList) {
+  let lastNode = linkedList.head;
+  while (lastNode.next !== null) {
+    lastNode = lastNode.next;
+  }
+  return lastNode;
+};
+
+function reverseList(linkedList) {};
+
+function thirdFromLast(linkedList) {};
+
+function middleOfList(linkedList) {};
+
+function cycleList(linkedList) {};
+
+function sortList(linkedList) {};
 
 module.exports = {
   main,
--- drills.md
+++ drills.md
@@ -1,3 +1,5 @@
 # Linked List drills
 
 ## Mystery Program
+
+This algorithm goes through a singly linked list and makes sure that no nodes in the list point to any previous node in the list. It has a runtime of O(n^2).
